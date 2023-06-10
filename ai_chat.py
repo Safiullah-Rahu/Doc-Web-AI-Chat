be688db35bf03df7f35372ccda73f116eab4611c
Safiullah Rahu <safiullahrahu1555@gmail.com>
2023-06-10 13:00:33 +0500

    Update ai_chat.py

--- ai_chat.py
+++ ai_chat.py
@@ -354,18 +354,17 @@ class Chatbot:
         self.vectors = vectors
 
 
-    _template = """Given the following conversation and a follow-up question, rephrase the follow-up question in japanese language to be a standalone question.
+    _template = """Given the following conversation and a follow-up question, rephrase the follow-up question to be a standalone question.
         Chat History:
         {chat_history}
         Follow-up entry: {question}
         Standalone question:"""
     CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
-    qa_template = """You are a friendly japanese conversational assistant, designed to answer questions in japanese language and chat with the user from a contextual file.
-        You receive data from a user's files and a question in japanese language, you must help the user find the information they need. 
-        Your answers must be user-friendly and respond to the user in the japanese language.
-        You will get questions and contextual information in japanese language and must answer in japanese language.
-        Never use english language in your response.
+    qa_template = """You are a friendly conversational assistant, designed to answer questions and chat with the user from a contextual file.
+        You receive data from a user's files and a question, you must help the user find the information they need. 
+        Your answers must be user-friendly and respond to the user.
+        You will get questions and contextual information.
         question: {question}
         =========
         context: {context}
@@ -472,20 +471,20 @@ def doc_search(temperature):
     else:
         search = SerpAPIWrapper()
         # Set up a prompt template which can interpolate the history
-        template_with_history = """You are SearchGPT, a professional search engine who provides informative answers to users. Answer the following questions in japanese as best you can. You have access to the following tools:
+        template_with_history = """You are SearchGPT, a professional search engine who provides informative answers to users. Answer the following questions as best you can. You have access to the following tools:
 
         {tools}
 
         Use the following format:
 
-        Question: the input question you must answer in japanese
+        Question: the input question you must answer
         Thought: you should always think about what to do
         Action: the action to take, should be one of [{tool_names}]
         Action Input: the input to the action
         Observation: the result of the action
         ... (this Thought/Action/Action Input/Observation can repeat N times)
         Thought: I now know the final answer
-        Final Answer: the final answer must be in japanese to the original input question
+        Final Answer: the final answer to the original input question
 
         Begin! Remember to give detailed, informative answers
 
